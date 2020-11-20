b0f2269f8af22eec73757ed298a4d2f4575e5b1a
PeshkaAbramovoi <lethme@ya.ru>
2020-11-20 13:16:49 +0400

    baloopa

--- assets/js/main_page.js
+++ assets/js/main_page.js
@@ -579,14 +579,14 @@ class MainPage extends React.Component
                     </div>
                 </footer>
 
-                <div id="ftco-loader" className="show fullscreen">
-                    <svg className="circular" width="48px" height="48px">
-                        <circle className="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4"
-                                stroke="#eeeeee"/>
-                        <circle className="path" cx="24" cy="24" r="22" fill="none" stroke-width="4"
-                                stroke-miterlimit="10" stroke="#4586ff"/>
-                    </svg>
-                </div>
+                {/*<div id="ftco-loader" className="show fullscreen">*/}
+                {/*    <svg className="circular" width="48px" height="48px">*/}
+                {/*        <circle className="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4"*/}
+                {/*                stroke="#eeeeee"/>*/}
+                {/*        <circle className="path" cx="24" cy="24" r="22" fill="none" stroke-width="4"*/}
+                {/*                stroke-miterlimit="10" stroke="#4586ff"/>*/}
+                {/*    </svg>*/}
+                {/*</div>*/}
             </div>
         );
     }
