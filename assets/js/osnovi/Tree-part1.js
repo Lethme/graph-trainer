58de9056a1add0922fb1f8c3d9b71896ac1d4169
Dima <semm0202@yandex.ru>
2020-12-22 00:37:27 +0300

    commit message nothing for usage

--- assets/js/osnovi/Tree-part1.js
+++ assets/js/osnovi/Tree-part1.js
@@ -3,28 +3,51 @@
  */
 
 
-
+/**
+ * class tree node
+ */
 class Node {
     constructor(value) {
         this.value = value;
+        //pointer to left child
         this.left = undefined;
+        //pointer to right child
         this.right = undefined;
     }
 
 }
+/**
+ * class tree
+ */
 class Tree {
+
     constructor() {
+
+        //pointer to tree root
         this.root = '';
-        this.search_resultes = '';
+
+        //contains search results 
+        this.search_results = '';
     }
 
+    /**
+     *
+     * @param tree_node as Node
+     * @param value as int
+     * @param prev_node as Node|undefined
+     */
     search_node(tree_node, value, prev_node = undefined) {
+
+        //if the element is found, write the result in search_results
         if (tree_node.value === value) {
-            this.search_resultes = { current: tree_node, prev: prev_node };
+            this.search_results = { current: tree_node, prev: prev_node };
         } else {
+
+            //search in left child
             if (tree_node.left) {
                 this.search_node(tree_node.left, value, tree_node);
             }
+            //search in right child
             if (tree_node.right) {
                 this.search_node(tree_node.right, value, tree_node);
             }
@@ -32,32 +55,57 @@ class Tree {
 
     }
 
+    /**
+     *
+     * @param node as Node
+     * @param value as int
+     */
     create_node(node, value) {
         if (!node.left || !node.right) {
+
             if (node.left) {
                 node.right = new Node(value)
             } else {
                 node.left = new Node(value);
             }
+
+            //add node in graph data
             nodes.push({ data: { id: value } });
+
+            //add edge in graph data
             edges.push({ data: { source: node.value, target: value } });
             draw_tree();
         }
     }
 
+    /**
+     *
+     * @param value as int
+     */
     create_root(value) {
         if (!this.root) {
+
             this.root = new Node(value);
             nodes.push({ data: { id: value } });
             draw_tree();
         }
     }
 
+    /**
+     *
+     * @param current_value as int
+     */
     handle_create_node(current_value) {
-        if ($('.attention-component').length) return;
+
+        if ($('.attention-component').length)
+            return;
+
         this.search_node(this.root, current_value);
-        let nodes = this.search_resultes;
+        let nodes = this.search_results;
+
         if (!nodes.current.left || !nodes.current.right) {
+
+            //create new window with text inputs
             new Attention.Prompt({
                 title: "Добавление элемента",
                 content: "",
@@ -65,20 +113,28 @@ class Tree {
                 submitText: "Добавить",
                 onSubmit: (component, value) =>{
                     let added_value = parseInt(value);
+
+                    //if value correct
                     if (added_value && !isNaN(added_value)) {
-                        this.search_resultes = '';
+                        this.search_results = '';
+
+                        //check twice
                         this.search_node(this.root, added_value);
-                        let is_twice_in_tree = this.search_resultes;
+                        let is_twice_in_tree = this.search_results;
                         if(!is_twice_in_tree.current){
                             document.tree.create_node(nodes.current, added_value, nodes.prev);
                         }
                         else{
+
+                            //throw error
                             new Attention.Alert({
                                 title: "Ошибка!",
                                 content: "Такая вершина уже имеется!"
                             });
                         }
                     } else {
+
+                        //throw error
                         new Attention.Alert({
                             title: "Ошибка!",
                             content: "Некорректный ввод!"
@@ -89,6 +145,11 @@ class Tree {
         }
     }
 
+
+    /**
+     * @param value as int
+     * @returns {number|Number}
+     */
     static get_node_index_in_array(value) {
         for (let node of nodes) {
             if (node.data.id == value)
@@ -96,8 +157,11 @@ class Tree {
         }
     }
 
-
-
+    /**
+     *
+     * @param value as int
+     * @returns {number|Number}
+     */
     static get_all_edges_indexes_target(value) {
         for (let edge of edges) {
             if (edge.data.target == value)
@@ -105,6 +169,10 @@ class Tree {
         }
     }
 
+    /**
+     *
+     * @param node as Node
+     */
     delete_node(node){
         if(node){
             this.delete_node(node.left);
@@ -112,23 +180,40 @@ class Tree {
         }
     }
 
+    /**
+     *
+     * @param current_value as int
+     */
     handle_delete_node(current_value) {
+
         this.search_node(this.root, current_value);
-        let cur_nodes = this.search_resultes;
+        let cur_nodes = this.search_results;
+
+        //if current node not a tree root
         if(cur_nodes.current && cur_nodes.current!==this.root){
+
+            //recurs delete left child
             if (cur_nodes.current && cur_nodes.current.left){
                 this.handle_delete_node(cur_nodes.current.left.value);
             }
+
+            //recurs delete right child
             if (cur_nodes.current && cur_nodes.current.right){
                 this.handle_delete_node(cur_nodes.current.right.value);
             }
+
+            //delete nodes
             if (cur_nodes.prev.right === cur_nodes.current)
                 cur_nodes.prev.right = undefined;
             else {
                 cur_nodes.prev.left = undefined;
             }
+
+            //delete node from a graph
             let node_index = Tree.get_node_index_in_array(current_value);
             nodes.splice(node_index, 1);
+
+            //delete edges from a graph
             let edge_index = Tree.get_all_edges_indexes_target(current_value);
             edges.splice(edge_index, 1);
         }
